81dc6ac7326f4d8bf8aa7eb1d70ab93c0757ce02
Марко <mgusanica@gmail.com>
2025-07-29 11:27:06 +0300

    fds

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<>
+    <RouterProvider>
+        <BrowserRouter><App/> </BrowserRouter>
+    </RouterProvider>
+</>
+);
